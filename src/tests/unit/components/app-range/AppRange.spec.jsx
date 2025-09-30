d285c36d6207e1eabdea7c9d145685dff2d5da24
Petro Sova <psova@softserveinc.com>
2025-09-30 16:43:11 +0300

    50: refactor unit tests

--- src/tests/unit/components/app-range/AppRange.spec.jsx
+++ src/tests/unit/components/app-range/AppRange.spec.jsx
@@ -3,9 +3,13 @@ import { describe, it, expect, vi } from 'vitest'
 import AppRange from '~/components/app-range/AppRange'
 
 describe('AppRange', () => {
-  const min = 13
-  const max = 78
+  const min = 0
+  const max = 100
   const handleChange = vi.fn()
+
+  beforeEach(() => {
+    render(<AppRange max={max} min={min} onChange={handleChange} />)
+  })
 
   it('should renders correctly snapshot', () => {
     const snapshot = render(
@@ -15,18 +19,14 @@ describe('AppRange', () => {
   })
 
   it('should renders correctly', () => {
-    render(<AppRange max={max} min={min} onChange={handleChange} />)
-
     const [minInput, maxInput] = screen.getAllByRole('textbox')
 
-    expect(minInput).toHaveValue('13')
-    expect(maxInput).toHaveValue('78')
+    expect(minInput).toHaveValue('0')
+    expect(maxInput).toHaveValue('100')
   })
 
   describe('AppRange - slider interaction', () => {
-    it('should update call onChange handler when slider is changed', async () => {
-      render(<AppRange max={100} min={0} onChange={handleChange} />)
-
+    it('should update call onChange handler when min slider is changed', async () => {
       const sliders = screen.getAllByRole('slider')
 
       fireEvent.change(sliders[0], { target: { value: 20 } })
@@ -36,9 +36,7 @@ describe('AppRange', () => {
       })
     })
 
-    it('should update call onChange handler when slider is changed', async () => {
-      render(<AppRange max={100} min={0} onChange={handleChange} />)
-
+    it('should update call onChange handler when max slider is changed', async () => {
       const sliders = screen.getAllByRole('slider')
 
       fireEvent.change(sliders[1], { target: { value: 25 } })
@@ -49,38 +47,21 @@ describe('AppRange', () => {
     })
   })
 
-  it('updates when input values are changed', async () => {
-    render(<AppRange max={max} min={min} onChange={handleChange} />)
-
-    const inputs = screen.getAllByRole('textbox')
-
-    fireEvent.change(inputs[0], { target: { value: '10' } })
-
-    await waitFor(() => {
-      expect(handleChange).toHaveBeenCalled()
-      expect(inputs[0]).toHaveValue('10')
-    })
-  })
-
-  it('constrains values on blur', () => {
-    render(<AppRange max={max} min={min} onChange={handleChange} />)
-
-    const inputs = screen.getAllByRole('textbox')
-
-    fireEvent.change(inputs[1], { target: { value: '999' } })
-    fireEvent.blur(inputs[1])
-
-    expect(inputs[1]).toHaveValue(String(max))
-  })
-
   describe('AppRange - input interaction', () => {
-    beforeEach(() => {
-      render(<AppRange max={100} min={0} onChange={handleChange} />)
-    })
-
     it('should render two input fields', () => {
       const inputs = screen.getAllByRole('textbox')
       expect(inputs).toHaveLength(2)
+    })
+
+    it('updates when input values are changed', async () => {
+      const inputs = screen.getAllByRole('textbox')
+
+      fireEvent.change(inputs[0], { target: { value: '10' } })
+
+      await waitFor(() => {
+        expect(handleChange).toHaveBeenCalled()
+        expect(inputs[0]).toHaveValue('10')
+      })
     })
 
     it('should call onChange when first input is changed', async () => {
@@ -112,70 +93,58 @@ describe('AppRange', () => {
     })
   })
 
-  describe('AppRange - invalid input handling', () => {
+  describe('AppRange - invalid and blur input handling', () => {
     const handleChange = vi.fn()
-    it('should not call onChange when input is changed with non-numeric value', () => {
-      render(<AppRange max={100} min={0} onChange={handleChange} />)
 
+    beforeEach(() => {
+      render(<AppRange max={max} min={min} onChange={handleChange} />)
+    })
+
+    it('should not call onChange when input is changed with non-numeric value', () => {
       const inputs = screen.getAllByRole('textbox')
       fireEvent.change(inputs[0], { target: { value: 'abc' } })
 
       expect(handleChange).not.toHaveBeenCalled()
     })
 
-    it('should not call onChange when input is cleared', () => {
-      render(<AppRange max={100} min={0} onChange={handleChange} />)
+    it('constrains values on blur', () => {
+      const inputs = screen.getAllByRole('textbox')
 
+      fireEvent.change(inputs[1], { target: { value: '999' } })
+      fireEvent.blur(inputs[1])
+
+      expect(inputs[1]).toHaveValue('100')
+    })
+
+    it('should not call onChange when input is cleared', () => {
       const inputs = screen.getAllByRole('textbox')
       fireEvent.change(inputs[0], { target: { value: '' } })
 
       expect(handleChange).not.toHaveBeenCalled()
     })
-  })
 
-  it('should call onChange with min number if first input is empty on blur', async () => {
-    const handleChange = vi.fn()
-    render(<AppRange max={100} min={0} onChange={handleChange} />)
+    it('should not update or call onChange when input is blurred with the same value', () => {
+      const inputs = screen.getAllByRole('textbox')
+      const minInput = inputs[0]
 
-    const inputs = screen.getAllByRole('textbox')
-    const minInput = inputs[0]
+      fireEvent.blur(minInput)
 
-    fireEvent.change(minInput, { target: { value: '' } })
+      expect(handleChange).not.toHaveBeenCalled()
 
-    fireEvent.blur(minInput)
-
-    await waitFor(() => {
-      expect(handleChange).toHaveBeenLastCalledWith([0, 100])
+      expect(minInput).toHaveValue('0')
     })
-  })
 
-  it('should update value to max when second input is blurred with value greater than max', async () => {
-    const handleChange = vi.fn()
-    render(<AppRange max={100} min={0} onChange={handleChange} />)
+    it('should update value to max when second input is blurred with value greater than max', async () => {
+      const inputs = screen.getAllByRole('textbox')
+      const maxInput = inputs[1]
 
-    const inputs = screen.getAllByRole('textbox')
-    const maxInput = inputs[1]
+      fireEvent.change(maxInput, { target: { value: '150' } })
 
-    fireEvent.change(maxInput, { target: { value: '150' } })
+      fireEvent.blur(maxInput)
 
-    fireEvent.blur(maxInput)
-
-    await waitFor(() => {
-      expect(maxInput).toHaveValue('100')
+      await waitFor(() => {
+        expect(maxInput).toHaveValue('100')
+      })
     })
-  })
-
-  it('should not update or call onChange when input is blurred with the same value', () => {
-    const handleChange = vi.fn()
-    render(<AppRange max={100} min={0} onChange={handleChange} />)
-
-    const inputs = screen.getAllByRole('textbox')
-    const minInput = inputs[0]
-
-    fireEvent.blur(minInput)
-
-    expect(handleChange).not.toHaveBeenCalled()
-
-    expect(minInput).toHaveValue('0')
   })
 })
