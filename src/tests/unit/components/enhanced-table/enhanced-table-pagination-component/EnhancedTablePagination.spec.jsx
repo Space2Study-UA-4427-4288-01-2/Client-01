d591daf89df16aa7636b9b880a75e70b15e9f297
ivannarybitska <ivannarybitska2809@gmail.com>
2025-10-02 12:43:53 +0300

    51: Add test for enhanced table pagination component.

---
+++ src/tests/unit/components/enhanced-table/enhanced-table-pagination-component/EnhancedTablePagination.spec.jsx
@@ -0,0 +1,46 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import EnhancedTablePagination from '~/components/enhanced-table/enhanced-table-pagination/EnhancedTablePagination'
+import { beforeEach, describe, expect } from 'vitest'
+import { vi } from 'vitest'
+
+describe('EnhancedTablePagination', () => {
+  let paginationMock
+  beforeEach(() => {
+    paginationMock = {
+      page: 1,
+      pageInput: 1,
+      rowsPerPage: 5,
+      pageCount: 4,
+      itemsCount: 20,
+      handleChangePage: vi.fn(),
+      handleChangeRowsPerPage: vi.fn(),
+      handleChangePageInput: vi.fn(),
+      handlePageSubmit: vi.fn()
+    }
+  })
+
+  it('should render first page', () => {
+    render(<EnhancedTablePagination pagination={paginationMock} />)
+
+    expect(
+      screen.getByText(
+        (content) => content.includes('1-5') && content.includes('20')
+      )
+    ).toBeInTheDocument()
+
+    const prevButton = screen.getByRole('button', { name: /previous/i })
+    expect(prevButton).toBeDisabled()
+  })
+
+  it('should change page from 1 to 2', () => {
+    render(<EnhancedTablePagination pagination={paginationMock} />)
+
+    const nextButton = screen.getByRole('button', { name: /next/i })
+    fireEvent.click(nextButton)
+
+    expect(paginationMock.handleChangePage).toHaveBeenCalledWith(
+      expect.any(Object),
+      2
+    )
+  })
+})
