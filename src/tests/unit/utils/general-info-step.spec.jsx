1e377b67c9fe6bda2c0b3d1cf6894aec8b71d313
Tetiana Kharkhalis <tetiana.s.shcherbata@gmail.com>
2025-10-20 18:53:14 +0300

    Changed test hierarchy

--- src/tests/unit/utils/general-info-step.spec.jsx
+++ src/tests/unit/utils/general-info-step.spec.jsx
@@ -12,17 +12,16 @@ describe('General Info Step Validations', () => {
       const result = firstName('John123')
       expect(result).toBe('common.errorMessages.nameAlphabeticOnly')
     })
+  })
+  describe('lastName validation', () => {
+    it('Should return error for names shorter than 3 characters', () => {
+      const result = lastName('Do')
+      expect(result).toBe('common.errorMessages.shortText')
+    })
 
-    describe('lastName validation', () => {
-      it('Should return error for names shorter than 3 characters', () => {
-        const result = lastName('Do')
-        expect(result).toBe('common.errorMessages.shortText')
-      })
-
-      it('Should return error for invalid characters', () => {
-        const result = lastName('Doe123')
-        expect(result).toBe('common.errorMessages.nameAlphabeticOnly')
-      })
+    it('Should return error for invalid characters', () => {
+      const result = lastName('Doe123')
+      expect(result).toBe('common.errorMessages.nameAlphabeticOnly')
     })
   })
 })
