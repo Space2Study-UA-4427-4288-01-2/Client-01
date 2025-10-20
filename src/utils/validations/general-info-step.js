e71278fc48b402b59547729f5725fe3d59432066
Tetiana Kharkhalis <tetiana.s.shcherbata@gmail.com>
2025-10-20 12:34:28 +0300

    Bug fixed, the length can't be less then 3, empty sp. deleted

--- src/utils/validations/general-info-step.js
+++ src/utils/validations/general-info-step.js
@@ -1,9 +1,20 @@
 import { nameField } from './common'
 
 export const firstName = (value) => {
-  return nameField(value)
+  {
+    if (value.trim().length >= 1 && value.trim().length < 3) {
+      return 'common.errorMessages.shortText'
+    }
+    console.log(value.trim().length)
+    return nameField(value.trim())
+  }
 }
 
 export const lastName = (value) => {
-  return nameField(value)
+  {
+    if (value.trim().length >= 1 && value.trim().length < 3) {
+      return 'common.errorMessages.shortText'
+    }
+    return nameField(value.trim())
+  }
 }
